cf57f87c83041bebaebcadaf478061ff3a5e01ff
Austin Garrett <agarret7@mit.edu>
2018-03-29 16:59:06 -0400

    Implement memory-inefficient version of algorithm.

--- knn/core.py
+++ knn/core.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 import matplotlib.pyplot as plt
+import time
 
 import numpy as np
 from scipy.spatial.distance import cdist
@@ -22,23 +24,30 @@ def knn(ref, qry, k):
     :param k: TODO
     :return: TODO
     """
-    return None
+    n, d = ref.size()
+    m, d = qry.size()
+    mref = ref.expand(m, n, d)
+    mqry = qry.expand(n, m, d).transpose(0,1)
+    print(mqry - mref)
+    dist = torch.sum((mqry - mref)**2, 2).squeeze()**0.5
+    dist, inds = torch.topk(dist, k, dim = 1, largest = False, sorted = False)
+    return inds, dist
 
 if __name__ == "__main__":
-    ref = torch.FloatTensor(100, 3).random_()
-    qry = torch.FloatTensor(10 , 3).random_()
-    dist = knn(ref, qry, 3)
-    print(dist)
+    torch.random.manual_seed(0)
 
-    m = 2500
-    p = 10
-    mp1 = torch.rand(m,p)
-    mp2 = torch.rand(m,p)
-    mmp1 = torch.stack([mp1]*m)
-    mmp2 = torch.stack([mp2]*m).transpose(0,1)
-    mm = torch.sum((mmp1-mmp2)**2,2).squeeze()
-    print(mm)
+    ref = Variable(torch.FloatTensor(50000, 3).random_()).cuda()
+    qry = Variable(torch.FloatTensor(5000, 3).random_()).cuda()
 
-    print("start")
-    print(np.sort(np.random.rand(50000000)))
-    print("stop")
+    t0 = time.time()
+    inds, dist = knn(ref, qry, 8)
+    print(time.time() - t0)
+
+    # t = 21
+    # ref_regional = ref[inds,:]
+    # test_point = qry[t,:].cpu().data.numpy()
+    # neighborhood = ref_regional[t,:,:].cpu().data.numpy()
+    # plt.scatter(ref[:,0], ref[:,1])
+    # plt.scatter(test_point[0], test_point[1], s = 100, c = 'green')
+    # plt.scatter(neighborhood[:,0], neighborhood[:,1], s = 100, c = 'red')
+    # plt.show()
